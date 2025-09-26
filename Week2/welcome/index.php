e159744031f300ed22843e63343ffd685f5021b3
Trung-Tin <tinntgcs220227@fpt.edu.vn>
2025-09-26 18:51:08 +0700

    add Week2 folder with calculator PHP project

--- Week2/welcome/index.php
+++ Week2/welcome/index.php
@@ -1,16 +1,16 @@
 <?php
-if (!isset($_POST['val1'])) {
-} else {
-    $val1 = $_POST['val1'];
-    $val2 = $_POST['val2'];
-    $calc = $_POST['calc'];
-
-    switch ($calc) {
-        case "add":$result = $val1 + $val2;
-        break;
-        case "sub":$result = $val1 - $val2;
-        break;
-    }
-    $output = "Calculation result:" . $result;
+if (!isset($_POST['val1'])) { include __DIR__ . '/templates/form.html.php'; exit; }
+$val1 = $_POST['val1'] ?? '';
+$val2 = $_POST['val2'] ?? '';
+$calc = $_POST['calc'] ?? 'add';
+if (!is_numeric($val1) || !is_numeric($val2)) { $error='Please enter valid numbers.'; include __DIR__ . '/templates/error.html.php'; exit; }
+$val1+=0; $val2+=0;
+switch ($calc) {
+  case 'add': $result=$val1+$val2; break;
+  case 'sub': $result=$val1-$val2; break;
+  case 'mul': $result=$val1*$val2; break;
+  case 'div': if($val2==0){$error='Division by zero.'; include __DIR__ . '/templates/error.html.php'; exit;} $result=$val1/$val2; break;
+  default: $result='Unknown'; 
 }
-?>
+$output = "Result: $result";
+include __DIR__ . '/templates/result.html.php';
