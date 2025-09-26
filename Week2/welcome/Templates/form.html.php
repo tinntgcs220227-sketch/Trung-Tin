ee3a2a85838b9355f4d26a73a5a2aa4ef79910d1
Trung-Tin <tinntgcs220227@fpt.edu.vn>
2025-09-26 18:48:27 +0700

    update error.html.php and result.html.php

--- Week2/welcome/Templates/form.html.php
+++ Week2/welcome/Templates/form.html.php
@@ -1,14 +1,37 @@
-<!doctype html><html><head><meta charset="utf-8"><title>Calc</title></head><body>
-<h1>Calc</h1>
-<form method="post">
-  <p>Value 1: <input name="val1"></p>
-  <p>Value 2: <input name="val2"></p>
-  <p>
-    <label><input type="radio" name="calc" value="add" checked> +</label>
-    <label><input type="radio" name="calc" value="sub"> −</label>
-    <label><input type="radio" name="calc" value="mul"> ×</label>
-    <label><input type="radio" name="calc" value="div"> ÷</label>
-  </p>
-  <button type="submit">Go</button>
-</form>
-</body></html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Basic Calculator</title>
+  <link rel="stylesheet" href="../public/style.css" />
+</head>
+<body>
+  <div class="container">
+    <h1>Basic Calculator</h1>
+
+    <form action="" method="post" class="card">
+      <label>
+        Value 1:
+        <input type="text" name="val1" required />
+      </label>
+
+      <label>
+        Value 2:
+        <input type="text" name="val2" required />
+      </label>
+
+      <fieldset class="ops">
+        <legend>Operation</legend>
+        <label><input type="radio" name="calc" value="add" checked /> Add (+)</label>
+        <label><input type="radio" name="calc" value="sub" /> Subtract (−)</label>
+        <label><input type="radio" name="calc" value="mul" /> Multiply (×)</label>
+        <label><input type="radio" name="calc" value="div" /> Divide (÷)</label>
+      </fieldset>
+
+      <button type="submit">Calculate</button>
+    </form>
+
+    <p class="note">Enter numbers, choose an operation, then press Calculate.</p>
+  </div>
+</body>
+</html>
